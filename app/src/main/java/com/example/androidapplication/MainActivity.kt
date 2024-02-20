7ec10e04e180e463ab07b501bd210de498c3e472
Raheem Chisman <raheem.chisman@microsoft.com>
2024-02-20 15:21:35 -0800

    Add button and view bindings

--- app/src/main/java/com/example/androidapplication/MainActivity.kt
+++ app/src/main/java/com/example/androidapplication/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.androidapplication
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.androidapplication.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    /* A ViewBinding provides a binding class, that can be used to access our views */
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        /* Initialize the binding * Here we would like to inflate a new layout */
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        /*Here we set the setContentView to our root layout of activity_main.xml */
+        setContentView(binding.root)
+        binding.button.setOnClickListener {
+            binding.textView2.visibility = View.VISIBLE
+            Toast.makeText(this, "Click Me Button Clicked",
+                Toast.LENGTH_SHORT).show()
+            binding.button1.visibility = View.VISIBLE
+            binding.button.visibility = View.GONE
+        }
+        binding.button1.setOnClickListener {
+            binding.textView2.visibility = View.GONE
+            Toast.makeText(this, "Reset Button Clicked", Toast.LENGTH_SHORT).show()
+            binding.button1.visibility = View.GONE
+            binding.button.visibility = View.VISIBLE
+        }
     }
+
 }
